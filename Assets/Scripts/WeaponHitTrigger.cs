0af530e6d8e7c1a91f2cf7331ded98d0c46d7f58
Sean Koh (RIT Student) <pxk8689@ad.rit.edu>
2019-01-17 16:44:24 -0500

    Update WeaponHitTrigger.cs

--- Assets/Scripts/WeaponHitTrigger.cs
+++ Assets/Scripts/WeaponHitTrigger.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponHitTrigger : MonoBehaviour {
+public class WeaponHitTrigger : MonoBehaviour
+{
+    public int dmg;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.isTrigger!=true && col.CompareTag("Enemy"))
+        {
+            col.SendMessageUpwards("Damage", dmg);
+        }
+    }
 }

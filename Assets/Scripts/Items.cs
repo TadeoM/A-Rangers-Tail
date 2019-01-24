16c71713bf2452a1c6fa1aa4c4c548cac734c8a8
seank0h <seankoh754@gmail.com>
2019-01-23 22:33:17 -0500

    Update Items.cs

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -6,7 +6,7 @@ public abstract class Items : MonoBehaviour {
     public string itemName;
     public int itemCost;
     public int durability;
-    public int damage;
+    
     public string type;
     public GameObject model;
     public bool stackable;
@@ -15,7 +15,7 @@ public abstract class Items : MonoBehaviour {
 		
 	}
     public int ItemDurability { get { return durability; } set { durability = value; } }
-    public int ItemDamage { get { return damage; } set { damage = value; } }
+    
     public string ItemName { get { return itemName; } set { itemName = value; } }
     public int ItemCost { get { return itemCost; } set { itemCost = value; } }
 

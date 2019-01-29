f0c3a9e88ef1b8e7bca7727fd98067f4c4be8b44
Gilexarr <tmenichel@gmail.com>
2019-01-28 20:06:04 -0500

    animation stuff for centpiede

--- Assets/Scripts/CentipedeAI.cs
+++ Assets/Scripts/CentipedeAI.cs
@@ -8,10 +8,18 @@ public class CentipedeAI : Enemy {
     {
         Lunge, Charge
     }
+    private float currTime;
+    private float animSpeed;
+    private Vector3 startAttackPos;
+    private Vector3 endAttackPos;
+    private float startHeight;
+    private float endHeight;
+    public CapsuleCollider attackCollider;
     private Attacks attackChosen; 
     private bool inAttackState;
-    private int attackTimer;
+    public float attackTimer;
     private Animator animator;
+    private int timesSwapped;
 
 	// Use this for initialization
 	public override void Start () {
@@ -19,6 +27,7 @@ public class CentipedeAI : Enemy {
         creatureType = CreatureType.Bug;
         inAttackState = false;
         animator = GetComponent<Animator>();
+        //attackCollider = GetComponentInChildren<CapsuleCollider>();
     }
 	
 	// Update is called once per frame
@@ -27,22 +36,58 @@ public class CentipedeAI : Enemy {
         {
             PerformAttack();
         }
+        else
+        {
+            AdjustColliders();
+            if (attackTimer > 0)
+            {
+                attackTimer -= Time.deltaTime;
+            }
+        }
         if(attackTimer <= 0)
         {
             animator.SetInteger("State", 0);
             inAttackState = false;
+            attackTimer = 0;
         }
-        if(attackTimer > 0)
-        {
-            attackTimer--;
-        }
+        
 	}
 
     protected override void PerformAttack()
     {
+
         inAttackState = true;
         attackChosen = Attacks.Lunge;
         animator.SetInteger("State", 2);
-        attackTimer = 60;
+        attackTimer = 38f * Time.deltaTime;
+        attackCollider.gameObject.SetActive(true);
+        startAttackPos = new Vector3(-0.14f, 0, 0);
+        endAttackPos = new Vector3(-0.8f, -0.3f, 0);
+        startHeight = 0.7f;
+        endHeight = 2;
+        animSpeed = 5f * Time.deltaTime ;
+        Debug.Log(animSpeed);
+        currTime = 0;
+        timesSwapped = 0;
+        AdjustColliders();
+    }
+
+    private void AdjustColliders()
+    {
+        if(attackTimer < 0.6)
+            currTime += animSpeed;
+
+        attackCollider.center = Vector3.Lerp(startAttackPos, endAttackPos, currTime);
+        attackCollider.height = Mathf.Lerp(startHeight, endHeight, currTime);
+        if(currTime > 1 && timesSwapped < 1)
+        {
+            currTime = 0;
+            Vector3 tempStartPos = startAttackPos;
+            startAttackPos = endAttackPos;
+            endAttackPos = tempStartPos;
+            float tempStartHeight = startHeight;
+            startHeight = endHeight;
+            endHeight = tempStartHeight;
+        }
     }
 }
